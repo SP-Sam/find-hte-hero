1388e58b135dc614106fc768becb6d1a4a51895f
Samuel Pereira <gmr.quest.10@gmail.com>
2023-06-23 10:58:34 -0300

    chore: organize initial files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,32 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// ** Next
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+// ** Global Styles
+import '@/styles/globals.css';
+
+// ** Fonts
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: '--font-poppins',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={poppins.className}>
+      <Head>
+        <title>Find the Hero</title>
+        <meta
+          name="description"
+          content="Encontre todos os heróis da Marvel e conheça os detalhes sobre eles"
+        />
+        <meta name="keywords" content="" />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </main>
+  );
 }
